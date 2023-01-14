737c6771e966c133f78c1b80b6e5e71ab7605a01
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-14 23:10:04 +0900

    1000

---
+++ src/main/kotlin/1000.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val value1 = sc.nextInt()
+    val value2 = sc.nextInt()
+    print("${value1+value2}")
+}
