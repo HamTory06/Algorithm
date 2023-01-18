46fe2034358781dc8476ef13e48109a779a59b8f
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-18 13:05:43 +0900

    2558

---
+++ src/main/kotlin/2558.kt
@@ -0,0 +1,9 @@
+import java.lang.reflect.Type
+import java.util.Scanner
+
+fun main(){
+    val cs = Scanner(System.`in`)
+    val a = cs.nextInt()
+    val b = cs.nextInt()
+    println("${a+b}")
+}
